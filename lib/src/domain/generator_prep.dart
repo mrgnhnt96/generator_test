1b3054e2d4f63433e7a34bffdaae55da1826d712
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-03 15:05:31 -0600

    style: add doc comments

--- lib/src/domain/generator_prep.dart
+++ lib/src/domain/generator_prep.dart
@@ -3,23 +3,32 @@ import 'package:build_test/build_test.dart';
 import 'package:generator_test/src/domain/domain.dart';
 import 'package:source_gen/source_gen.dart';
 
+/// Prepares the generators and files for testing
 class GeneratorPrep {
+  /// prepares the generator and file for testing
   GeneratorPrep(
     this.fileNames,
     Generator generator, {
     this.compareWithOutput = false,
   }) : generators = [generator];
 
+  /// prepares the generators and files for testing
   GeneratorPrep.multi(
     this.fileNames,
     this.generators, {
     this.compareWithOutput = false,
   });
 
+  /// the names of the files to test
   final List<String> fileNames;
+
+  /// the generators to test
   final List<Generator> generators;
+
+  /// whether to compare the output file with the output of the generator
   final bool compareWithOutput;
 
+  /// the builder for the test
   Builder get builder {
     return PartBuilder(generators, '.g.dart');
   }
@@ -36,10 +45,12 @@ class GeneratorPrep {
     });
   }
 
+  /// the input files for the test
   Map<String, String> get inputs {
     return _puts(_inContent);
   }
 
+  /// the output files for the test
   Map<String, String> get outputs {
     return compareWithOutput ? _puts(_outContent) : {};
   }
@@ -57,16 +68,19 @@ class GeneratorPrep {
 
   MultiPackageAssetReader? _reader;
 
+  /// the asset reader for the test
   Future<MultiPackageAssetReader> get reader async {
     return _reader ??= await PackageAssetReader.currentIsolate();
   }
 
+  /// checks if the path is a provided input file
   bool isInput(String path) {
     final file = path.replaceAll(Content.lib, '');
 
     return fileNames.contains(file);
   }
 
+  /// tests the generator
   Future<void> test() async {
     await testBuilder(
       builder,
