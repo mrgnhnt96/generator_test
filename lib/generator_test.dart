16412e75a39436b9839f1826ea1b63f26c9377c5
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-07 08:23:03 -0600

    feat: export test config, and input/output helpers

--- lib/generator_test.dart
+++ lib/generator_test.dart
@@ -1,4 +1,6 @@
 library generator_test;
 
-export 'src/domain/domain.dart' show GeneratorPath;
+export 'src/domain/domain.dart' show GeneratorPath, TestConfig;
 export 'src/generator_tests.dart';
+export 'src/util/util.dart'
+    show inputContent, outputContent, outputContentMultiGen;
