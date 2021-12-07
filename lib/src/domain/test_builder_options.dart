270dd9150e42e49f1bd76ec81bf98abdd8ba9ce0
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-07 08:05:37 -0600

    refactor: isolate test builder options

---
+++ lib/src/domain/test_builder_options.dart
@@ -0,0 +1,9 @@
+import 'package:build/build.dart';
+
+/// {@template test_builder_options}
+/// Creates a [BuilderOptions] from the given [config]
+/// {@endtemplate}
+class TestBuilderOptions extends BuilderOptions {
+  /// {@macro test_builder_options}
+  TestBuilderOptions(Map<String, dynamic> config) : super(config);
+}
