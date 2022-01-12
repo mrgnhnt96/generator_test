f6b20cbf6daa386e874921e3c5f5d28688c787a1
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-11 19:51:05 -0700

    refactor: remove unneeded file

--- lib/src/domain/test_builder_options.dart
+++
@@ -1,9 +0,0 @@
-import 'package:build/build.dart';
-
-/// {@template test_builder_options}
-/// Creates a [BuilderOptions] from the given [config]
-/// {@endtemplate}
-class TestBuilderOptions extends BuilderOptions {
-  /// {@macro test_builder_options}
-  const TestBuilderOptions(Map<String, dynamic> config) : super(config);
-}
