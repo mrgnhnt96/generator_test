04e8f9db6d306a4e4bd50ea288aa2fedb1f41ab0
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-11 19:50:39 -0700

    refactor: create get content mixin

--- lib/src/domain/content.dart
+++ lib/src/domain/content.dart
@@ -1,4 +1,6 @@
-import 'package:generator_test/src/util/util.dart';
+import 'dart:io';
+
+import 'package:generator_test/src/domain/generator_path.dart';
 
 /// Input/fixture type for [Content] of the generator
 enum PutType {
@@ -12,9 +14,9 @@ enum PutType {
 /// {@template content}
 /// The contents of a local file
 /// {@endtemplate}
-class Content {
+class Content with GetContentMixin {
   /// {@macro content}
-  Content(
+  const Content(
     this.fileName, {
     required this.addPart,
     required this.directory,
@@ -53,19 +55,17 @@ class Content {
   /// the file to use as the source of the file
   final String fromFileName;
 
-  String? _content;
-
   /// The contents of the file as a string
   String get content {
     if (type == PutType.fixture) {
-      return _content ??= fixtureContent(
+      return fixtureContent(
         fileName,
         fromFileName: fromFileName,
         dirPath: directory,
       );
     }
 
-    return _content ??= inputContent(
+    return inputContent(
       fromFileName,
       addPart: addPart,
       extension: extension(getfixture: true),
@@ -117,5 +117,96 @@ class Content {
     }
 
     return ext;
+  }
+}
+
+/// Methods to get the contents of a file
+mixin GetContentMixin {
+  /// Retrieves the file content from the [GeneratorPath.input]/[fileName].dart file.
+  ///
+  /// [addPart] adds `part '[fileName].g.dart';' to the file after imports
+  String inputContent(
+    String fileName, {
+    bool addPart = false,
+    required String dirPath,
+    required String extension,
+  }) {
+    final path = '$dirPath/$fileName.dart';
+
+    final part = "part '$fileName$extension';";
+
+    final content = _getFileContent(
+      path,
+      part: part,
+      addPart: addPart,
+    );
+
+    return content;
+  }
+
+  /// Retrieves the file content from the [GeneratorPath.fixture]/[fileName].dart file.
+  ///
+  /// Automatically adds
+  /// - `part of '[fileName].dart';`
+  /// - `// GENERATED CODE - DO NOT MODIFY BY HAND`
+  /// - Generator's name (`T`) comment
+  String fixtureContent(
+    String fileName, {
+    required String fromFileName,
+    required String dirPath,
+  }) {
+    final path = '$dirPath/$fromFileName.dart';
+
+    final fixture =
+        _getFileContent(path, part: "part of '$fileName.dart';\n\n");
+
+    return fixture;
+  }
+
+  /// gets the file's content from the given [path].
+  String _getFileContent(
+    String path, {
+    required String part,
+    bool addPart = true,
+  }) {
+    String getContent() {
+      final file = File(path);
+
+      if (!file.existsSync()) {
+        throw Exception('File not found: $path');
+      }
+
+      final content = file.readAsStringSync();
+
+      if (!addPart) {
+        return content;
+      }
+
+      final partRegex = RegExp(r"part .*';[\r\n]+");
+
+      // check for part with specific extension
+      if (content.contains(partRegex)) {
+        return content.replaceFirst(partRegex, part);
+      }
+
+      if (!content.contains(RegExp('import .*;'))) {
+        return [part, content].join();
+      }
+
+      final lines = content.split('\n');
+      final indexAfterImport = lines.indexWhere(
+        (line) =>
+            !line.startsWith(RegExp(r'^(import|//|\s)', multiLine: true)) &&
+            line.isNotEmpty,
+      );
+
+      lines.insert(indexAfterImport, part);
+
+      return lines.join('\n');
+    }
+
+    final result = getContent();
+
+    return result;
   }
 }
